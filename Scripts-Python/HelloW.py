e323567a6e58cf73c510784453b6f5540a9f9c64
Felipe Fontenele <felipefontenele1317@gmail.com>
2020-04-10 22:21:32 -0300

    Realização do desafio 79 do curso de python

--- Scripts-Python/HelloW.py
+++
@@ -1,8 +0,0 @@
-palavras = 'Mano', 'Battlefield', 'Coisa Nossa', 'Guaraná', 'Hunter', 'Magica', 'Milharina', 'Cuscuz', 'Cabelo'
-
-for palavra in palavras:
-    print(f'A palavra {palavra} tem as vogais ', end='')
-    for letra in palavra:
-        if letra.lower() in 'aeiou':
-            print(letra, end=' ')
-    print()
