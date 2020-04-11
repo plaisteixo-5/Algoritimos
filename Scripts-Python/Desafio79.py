49925ede06568ac178124aae5ac9c75cd9f0b174
Felipe Fontenele <felipefontenele1317@gmail.com>
2020-04-11 05:19:23 -0300

    Realização dos desafios da aula de listas do Guanabara

--- Scripts-Python/Desafio79.py
+++ Scripts-Python/Desafio79.py
@@ -1,26 +1,15 @@
-numeros = []
-posições = []
-maior = 0
-menor = 999
+num = list()
+n = 0
 
-for c in range(0,5):
-    numeros.append(int(input('DIgite um número: ')))
-    if c == 0:
-        maior = menor = numeros[c]
+while True:
+    n = int(input('Digite um número: '))
+    if n not in num:
+        num.append(n)
+        print('Número adicionado!')
     else:
-        if numeros[c] > maior:
-            maior = numeros[c]
-        elif numeros[c] < menor:
-            menor = numeros[c]
-
-print(f'Você digitou os valores {numeros}')
-print(f'O maior valor digitado foi {maior} nas posições ', end='')
-for i, v in enumerate(numeros):
-    if v == maior:
-        print(f'{i} ', end='')
-
-print()
-print(f'O menor valor digitado foi {menor} mas posiçõrd ', end='')
-for i, v in enumerate(numeros):
-    if v == menor:
-        print(f'{i} ', end='')
+        print('O número já está na lista. Tente outro!')
+    c = str(input('Deseja continuar? [S/N]'))
+    if c in 'Nn':
+        break
+num.sort()
+print(f'Os valor digitados foram {num}')
