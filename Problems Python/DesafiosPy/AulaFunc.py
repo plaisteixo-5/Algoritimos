d6e39a3e6f6b3713ed42879f281e96a59fe5ca59
Felipe Fontenele <felipefontenele1317@gmail.com>
2020-04-18 16:30:49 -0300

    Desafios da aula de funções

---
+++ Problems Python/DesafiosPy/AulaFunc.py
@@ -0,0 +1,26 @@
+# def mensagem(algo):
+#     print('-'*10)
+#     print(algo)
+#     print('-'*10)
+
+
+# mensagem('Funcionou')
+
+# def soma(*num): 
+#ELE FEZ UMA TUPLA AQUI#
+#     sorted(num)
+#     print(sorted(num))
+
+# soma(3,4,7,2)
+
+# def dobra(*lst):
+#     for c in range(0,len(lst)):
+#         pos = 0
+#         while pos < len(lst[c]):
+#             lst[c][pos] *= 2
+#             pos += 1
+
+# valores = [6,4,3,34,4]
+# valores2 = [1,3,4,5,6]
+# dobra(valores,valores2)
+# print(valores,valores2)
