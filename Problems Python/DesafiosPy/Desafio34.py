0b1cf986f152e56531527bc7b260b7a590032b88
Felipe Fontenele <felipefontenele1317@gmail.com>
2020-04-18 18:59:47 -0300

    Teste

--- Problems Python/DesafiosPy/Desafio34.py
+++ Problems Python/DesafiosPy/Desafio34.py
@@ -3,4 +3,3 @@ if(s > 1250):
     print('Seu salário irá de {:.2f} para {:.2f}'.format(s,(s*0.1)+s))
 else:
     print('Seu salário irá de {:.2f} para {:.2f}'.format(s,(s*0.15)+s))
-print('Testando terminal do lixunis')
